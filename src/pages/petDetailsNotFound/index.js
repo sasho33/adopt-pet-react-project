5f0855c4111dc1093e687dc8f1e044b047ec679f
sasho33 <sasho3333333@gmail.com>
2023-10-21 01:38:18 +0100

    route finish

--- src/pages/petDetailsNotFound/index.js
+++ src/pages/petDetailsNotFound/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 // import useNavigate here.
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const PetDetailsNotFound = () => {
   // get the navigate function from useNavigate
-  const navigate = 'REPLACE ME';
+  const navigate = useNavigate();
 
   const goHome = () => {
     // Go home!
+    navigate('/');
   };
 
   return (
